07fd74025c76ba2e55b46d62c8ed68425c69414f
Leo Salat <leo.salat@gmail.com>
2017-01-31 09:52:08 -0800

    add mancala board logic

--- w2d2/skeleton/lib/board.rb
+++ w2d2/skeleton/lib/board.rb
@@ -1,17 +1,43 @@
+require 'byebug'
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1, @name2 = name1, name2
+    @cups = Array.new(14) { [] }
+    @stores = {}
+    stores
+    place_stones
+  end
+
+  def stores
+    @stores[@name1] = 6
+    @stores[@name2] = 13
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, i|
+      next if i == 6 || i == 13
+      cups[i] += Array.new(4) { :stone }
+    end
   end
 
   def valid_move?(start_pos)
+    unless ((0..5).to_a + (7..12).to_a).include?(start_pos)
+      raise 'Invalid starting cup'
+    end
+    true
   end
 
   def make_move(start_pos, current_player_name)
+    stones = empty_cup(start_pos)
+    cup = start_pos
+    until stones.empty?
+      cup += 1
+      next if (cup + 7) % 14 == @stores[current_player_name]
+      cups[cup % 14] << stones.pop
+    end
+    render
   end
 
   def next_turn(ending_cup_idx)
@@ -30,5 +56,18 @@ class Board
   end
 
   def winner
+  end
+
+  def empty_cup(index)
+    stones = []
+    until cups[index].empty?
+      stones << cups[index].pop
+    end
+
+    stones
+  end
+
+  def distribute_stones(start_pos, stones, current_player_name)
+
   end
 end
