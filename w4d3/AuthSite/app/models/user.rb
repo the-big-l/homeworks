33233d58b9729ec577882a9257da87652c1b180f
Leo Salat <leo.salat@gmail.com>
2017-02-15 20:16:40 -0800

    add sessions

--- w4d3/AuthSite/app/models/user.rb
+++ w4d3/AuthSite/app/models/user.rb
@@ -11,9 +11,45 @@
 #
 
 class User < ActiveRecord::Base
-  validates :username, :session_token, presence: true
+  attr_reader :password
+
+  validates :username, uniqueness:
+    { message: "Username already taken" }
+  validates :session_token, presence: true
   validates :password_digest, presence:
     { message: "Password can't be blank" }
   validates :password, length: { minimum: 6, allow_nil: true }
+  before_validation :ensure_session_token
 
+  def self.find_by_credentials(username, password)
+    user = User.find(username: username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+
+  end
+
+  def password=(pwd)
+    @password = pwd
+    self.password_digest = BCrypt::Password.create(pwd)
+  end
+
+  def is_password?(pwd)
+    BCrypt::Password.new(self.password_digest).is_password?(pwd)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
 end
