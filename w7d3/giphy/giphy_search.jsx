0d0abaed3ea5bdcf44d61bf87850fdf980da3a4c
Leo Salat <leo.salat@gmail.com>
2017-03-08 08:55:50 -0800

    add search util to giphy search

---
+++ w7d3/giphy/giphy_search.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+import {getSearchGiphys} from './util/api_util';
+import {receiveSearchGiphys} from './actions/giphy_actions';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+  window.store = store;
+  window.getSearchGiphys = getSearchGiphys;
+  window.receiveSearchGiphys = receiveSearchGiphys;
+
+});
