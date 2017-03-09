bccf5303982c16fd1687136019b48952d7b3ccfc
Leo Salat <leo.salat@gmail.com>
2017-03-08 17:57:53 -0800

    add routes

--- w7d4/rainbow_routes/entry.jsx
+++ w7d4/rainbow_routes/entry.jsx
@@ -63,7 +63,15 @@ Rainbow = withRouter(Rainbow);
 
 const routes = (
   <Route path="/" component={Rainbow}>
-    // your routes here
+    <Route path="red" component={Red}>
+      <Route path="orange" component={Orange} />
+      <Route path="yellow" component={Yellow} />
+    </Route>
+    <Route path="green" component={Green} />
+    <Route path="blue" component={Blue}>
+      <Route path="indigo" component={Indigo} />
+    </Route>
+    <Route path="violet" component={Violet} />
   </Route>
 );
 
